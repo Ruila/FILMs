0cd6eaafee25229e7ad3e18134102b7a452cba75
mengru <4momolmr@gmail.com>
2020-08-09 12:21:37 +0800

    add title viewcount outlet in videopage

--- FILMs/Controller/VideoViewCellViewController.swift
+++ FILMs/Controller/VideoViewCellViewController.swift
@@ -10,22 +10,34 @@ import UIKit
 
 class VideoViewCellViewController: UIViewController, YTPlayerViewDelegate {
     
-
-    @IBOutlet var playerview: YTPlayerView!
-    var videoId: String?
     
-    let playvarsDic = ["controls": 1, "playsinline": 1, "autohide": 1, "showinfo": 1, "autoplay": 0, "modestbranding": 1]
+    @IBOutlet var playerview: YTPlayerView!
+    @IBOutlet weak var ViewCount: UILabel!
+    @IBOutlet weak var VideoTitle: UILabel!
+    
+    var videoId: String?
+    var videoTitle:String?
+    var viewCount: String?
+    
+    
+    let playvarsDic = ["controls": 1, "playsinline": 1, "autohide": 1, "showinfo": 1, "autoplay": 1, "modestbranding": 1]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         Display()
-       
+        
         // Do any additional setup after loading the view.
     }
     
     func Display(){
         playerview.delegate = self
         playerview.load(withVideoId: self.videoId!, playerVars: playvarsDic)
+        ViewCount.text = "觀看次數：" + self.viewCount!
+        VideoTitle.text = self.videoTitle
+        //文字過長時的現實方式
+        VideoTitle.lineBreakMode = NSLineBreakMode.byWordWrapping;
+        //文字框是否允許多行（佈局相關）
+        VideoTitle.numberOfLines = 0;
     }
     
 }
