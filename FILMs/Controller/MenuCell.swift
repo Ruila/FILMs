c9a2109590e750a86932ba3ae40ccb0f6c9e85b2
mengru <4momolmr@gmail.com>
2020-08-14 19:53:03 +0800

    menbar click effect can work, problem is god damn I don't know what is required init

--- FILMs/Controller/MenuCell.swift
+++ FILMs/Controller/MenuCell.swift
@@ -12,27 +12,42 @@ class MenuCell: UICollectionViewCell {
     
     let imageView: UIImageView = {
         let iv = UIImageView()
-        iv.image = UIImage(named: "home")
+        iv.image = UIImage(named: "home")?.withRenderingMode(.alwaysTemplate)
+        iv.tintColor = UIColor(red: 91/255, green: 14/255, blue: 13/255, alpha: 1)
         return iv
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        print("ffff")
+        
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        
+        
+        
+        self.addSubview(imageView)
+        self.addConstraintsWithFormat("H:[v0(25)]", views: imageView)
+        self.addConstraintsWithFormat("V:[v0(25)]", views: imageView)
+        self.addConstraint(NSLayoutConstraint(item: imageView, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1, constant: 0))
+        self.addConstraint(NSLayoutConstraint(item: imageView, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0))
+        
+        self.tintColor = UIColor(red: 91/255, green: 14/255, blue: 13/255, alpha: 1)
     }
     
     
     override var isHighlighted: Bool{
         didSet {
-            print("123")
+            imageView.tintColor = self.isHighlighted ? UIColor.white : UIColor(red: 91/255, green: 14/255, blue: 13/255, alpha: 1)
         }
     }
     
-    
+    override var isSelected: Bool{
+        didSet {
+            imageView.tintColor = self.isSelected ? UIColor.white : UIColor(red: 91/255, green: 14/255, blue: 13/255, alpha: 1)
+        }
+    }
     
     
     
